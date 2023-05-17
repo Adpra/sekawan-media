6fd8c9b46a06b7e4c37abe8c44c43cd0d37f88b8
Adi Pra <adi.pra41@gmail.com>
2023-05-17 22:37:17 +0700

    AdminLTE

---
+++ database/migrations/2023_05_17_120230_create_vehicle_usages_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('vehicle_usages', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('vehicle_id')->index();
+            $table->bigInteger('user_id')->index();
+            $table->float('fuel_consumption');
+            $table->date('service_schedule');
+            $table->date('usage_date');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('vehicle_usages');
+    }
+};
